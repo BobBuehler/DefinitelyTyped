9626f7acff614966e9e069e2d6d90a240cff959e
Bob Buehler <BobBuehler@users.noreply.github.com>
2017-09-01 09:58:29 -0500

    Add .text to Include
    See documentation here https://github.com/producthunt/chai-enzyme#textstr
    {code}expect(wrapper.find('#child')).to.contain.text('Te'){code}

--- types/chai-enzyme/index.d.ts
+++ types/chai-enzyme/index.d.ts
@@ -26,6 +26,12 @@ declare namespace Chai {
          * @param code
          */
         (selector: EnzymeSelector): Assertion;
+
+        /**
+         * Assert that the given wrapper contains the supplied text:
+         * @param str
+         */
+        text(str?: string): Assertion;
     }
     interface Assertion {
         /**
